e5c6e27ebe805b35b85f9dfca589d3980a1a55d2
ArfinHasib <arfinhasib21375@gmail.com>
2023-11-29 09:36:28 +0600

    using keyboard event listeners to check for key presses

--- index.js
+++ index.js
@@ -6,45 +6,55 @@ for (let i = 0; i < document.querySelectorAll('.drum').length; i++) {
    function handleClick() {
       let buttonInnerHTML = this.innerHTML;
 
-      switch (buttonInnerHTML) {
-         case 'w':
-            let tom1 = new Audio('sounds/tom-1.mp3');
-            tom1.play();
-            break;
+      makeSound(buttonInnerHTML);
+   }
+}
 
-         case 'a':
-            let tom2 = new Audio('sounds/tom-2.mp3');
-            tom2.play();
-            break;
+// Detecting keyboard press
+document.addEventListener('keypress', function (event) {
+   makeSound(event.key);
+});
 
-         case 's':
-            let tom3 = new Audio('sounds/tom-3.mp3');
-            tom3.play();
-            break;
+// make sound
+function makeSound(key) {
+   switch (key) {
+      case 'w':
+         let tom1 = new Audio('sounds/tom-1.mp3');
+         tom1.play();
+         break;
 
-         case 'd':
-            let tom4 = new Audio('sounds/tom-4.mp3');
-            tom4.play();
-            break;
+      case 'a':
+         let tom2 = new Audio('sounds/tom-2.mp3');
+         tom2.play();
+         break;
 
-         case 'j':
-            let snare = new Audio('sounds/snare.mp3');
-            snare.play();
-            break;
+      case 's':
+         let tom3 = new Audio('sounds/tom-3.mp3');
+         tom3.play();
+         break;
 
-         case 'k':
-            let crash = new Audio('sounds/crash.mp3');
-            crash.play();
-            break;
+      case 'd':
+         let tom4 = new Audio('sounds/tom-4.mp3');
+         tom4.play();
+         break;
 
-         case 'l':
-            let kick = new Audio('sounds/kick-bass.mp3');
-            kick.play();
-            break;
+      case 'j':
+         let snare = new Audio('sounds/snare.mp3');
+         snare.play();
+         break;
 
-         default:
-            console.log('Wrong Keyword');
-            break;
-      }
+      case 'k':
+         let crash = new Audio('sounds/crash.mp3');
+         crash.play();
+         break;
+
+      case 'l':
+         let kick = new Audio('sounds/kick-bass.mp3');
+         kick.play();
+         break;
+
+      default:
+         console.log(buttonInnerHTML);
+         break;
    }
 }
